36b93cc6a05e1a9dfb39030f3fdabb0366d3b984
jwijngaarde <ajwijngaarde@gmail.com>
2021-09-01 01:03:38 +0200

    16th commit

---
+++ nodejs_basis/index.js
@@ -0,0 +1,9 @@
+let voorNaam = 'Armand';
+let achterNaam = 'Wijngaarde';
+let mijnLeeftijd = 52;
+let optellen = 4 + 4;
+let telop = '4' + '4';
+let deling = 40/10;
+let modulo = 40%10;
+typeof mijnLeeftijd('52')
+console.log(mijnLeeftijd);
