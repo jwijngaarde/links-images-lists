042995da64cc68605e4ab933730e3c54404fbb2c
jwijngaarde <ajwijngaarde@gmail.com>
2021-09-01 02:02:25 +0200

    19th commit

---
+++ W1D2-3/script.js
@@ -0,0 +1,15 @@
+console.log("Hallo Winc Academy studenten")
+
+//"Dit is een grote som"
+/*let vermenigvuldiging = 1230941 * 1823794;
+console.log(vermenigvuldiging)
+
+let deling = 637263 / 54;
+console.log(deling)*/
+
+vraag 4) het comment gedeelte is niet te zien in de uitkomst omdat het dient als leidende tekst vor de lezer met oa instructies
+
+vraag 5) het uitgecommente gedeelte word geskipt omdat het nu geen code is
+
+vraag 6) het komt niet te zien
+
