6dcee6942f83ed6c12edaa751b65f8c01014cbac
jwijngaarde <ajwijngaarde@gmail.com>
2021-09-01 05:36:31 +0200

    20th commit

---
+++ if_else_basis/kroegBaas.js
@@ -0,0 +1,11 @@
+let age = 19;
+let isFemale = false;
+let driverStatus = 'karel';
+if (age > 18 && isFemale == true){
+    console.log("U mag naar binnen!");
+    } if(driverStatus == 'bob'){
+    console.log("U mag autorijden!");
+} else if(driverStatus !== 'bob'){
+    console.log("U mag geen autorijden!");
+    }
+    
