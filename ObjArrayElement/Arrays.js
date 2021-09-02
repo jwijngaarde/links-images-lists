69b72cb37bdebc004dd2ed533b9e4c791a00e855
jwijngaarde <ajwijngaarde@gmail.com>
2021-09-02 21:22:02 +0200

    22th commit

---
+++ ObjArrayElement/Arrays.js
@@ -0,0 +1,20 @@
+let kleuren = ['groen', 'blauw', 'rood'];
+kleuren[3] = 'geel';
+kleuren[4] = 5;
+kleuren.push({greeting: "Hi ik ben een object"});
+console.log(kleuren);
+
+// Lengte van de Array
+console.log(kleuren.length);
+
+// Log het eerste element
+console.log(kleuren[0]);
+
+//
+console.log(kleuren[2]);
+
+console.log(kleuren[5]);
+
+
+// Voeg  vierde element toe
+console.log(kleuren);
---
+++ ObjArrayElement/object.js
@@ -0,0 +1,21 @@
+
+let person = {
+    name: 'Armand',
+    age: 52
+};
+
+
+console.log(person);
+
+// Dot Notation
+console.log(person.name);
+console.log(person.age);
+
+// Bracket Notation
+let selection = 'name';
+person['name'] = 'Jeffrey';
+console.log(person.name);
+
+//log evaluations
+let evaluations = [7, 10, 9];
+console.log(evaluations);
