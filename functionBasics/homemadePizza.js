cb72ff88872b7f343c387f989e08cf69cb6e4366
jwijngaarde <ajwijngaarde@gmail.com>
2021-09-03 18:38:42 +0200

    23th commit

---
+++ functionBasics/homemadePizza.js
@@ -0,0 +1,46 @@
+// My first function
+const makehomemadePizza = function() {
+    console.log("In large bowl, dissolve yeast and sugar in water");
+    console.log("Add oil and salt");
+    console.log("Stir in flour, 1 cup at a time, until a soft dough forms");
+    console.log("Turn onto a floured surface; knead until smooth and elastic");
+    console.log("Cover and let rise in a warm place until doubled");
+    console.log("Meanwhile, cook beef and onion over medium heat");
+    console.log("Punch down dough; divide in half");
+    console.log("Press each half into a greased. pizza pan");
+    console.log("Combine the tomato sauce, oregano and basil");
+    console.log("Spread over each crust.");
+    console.log("Top with beef mixture, green pepper and cheese");
+    console.log("Bake at 200°c for 25-30 minutes until crust is lightly browned");
+};
+
+makehomemadePizza();
+makehomemadePizza();
+
+const makeSushi = function() {
+    console.log("Soak rice for 4 hours");
+    console.log("Drain rice and cook in a rice cooker with 2 cups of water");
+    console.log(
+        "After rice is cooked, mix in 6 tablespoons rice vinegar to the hot rice"
+    );
+    console.log("Place 1 sheet of seaweed on bamboo mat");
+    console.log("Press a thin layer of cool rice on the seaweed");
+    console.log(
+        "Leave at least 1.25cm top and bottom edge of the seaweed uncovered"
+    );
+    console.log("Dot some wasabi on the rice");
+    console.log("Arrange cucumber, avocado and smoked salmon to the rice");
+    console.log(
+        "Position them about 2.5 cm away from the bottom edge of the seaweed"
+    );
+    console.log("Slightly wet the top edge of the seaweed");
+    console.log(
+        "Roll from bottom to the top edge with the help of the bamboo mat tightly"
+    );
+    console.log("Cut roll into 8 equal pieces and serve");
+    console.log("Repeat for other rolls");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
